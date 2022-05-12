41cb5e09b2d45653fb828a63773cc09e1711cb5b
Catherine Tostes <catherinetostes@gmail.com>
2022-05-11 22:11:53 -0300

    Add app files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/authentication/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
